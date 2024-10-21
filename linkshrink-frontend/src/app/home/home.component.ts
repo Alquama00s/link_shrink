531d9cd2676348a602c7b4d5e0830ef5038dfb78
Alquama <alquamasalim000@gmail.com>
2024-10-21 13:24:03 +0530

    form implemented

--- linkshrink-frontend/src/app/home/home.component.ts
+++ linkshrink-frontend/src/app/home/home.component.ts
@@ -4,17 +4,48 @@ import { AgGridAngular } from 'ag-grid-angular';
 import { ColDef } from 'ag-grid-community';
 import { ButtonComponent } from '../sub-components/grid-components/button/button.component';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, AgGridAngular,MatIconModule],
+  imports: [
+    NavComponent,
+    AgGridAngular,
+    MatIconModule,
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  showForm = false;
+  longUrl = '';
+  shortUrl = '';
+
   deleteRow(data: any) {
     this.rowData = this.rowData.filter((d) => d.id !== data.id);
+  }
+
+  closeForm() {
+    this.showForm = false;
+  }
+  openForm() {
+    this.showForm = true;
+  }
+
+  addUrl() {
+    console.log(this.shortUrl)
+    console.log(this.longUrl)
+    this.closeForm()
   }
 
   rowData = [
@@ -33,7 +64,7 @@ export class HomeComponent {
       field: 'actions',
       cellRenderer: ButtonComponent,
       flex: 1,
-      onCellClicked:(event)=> this.deleteRow(event.data),
+      onCellClicked: (event) => this.deleteRow(event.data),
     },
   ];
 }
