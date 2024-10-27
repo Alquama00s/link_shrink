eed856688db5bf9bbe8bd9c241305c52573a6e24
Alquama <alquamasalim000@gmail.com>
2024-10-27 18:26:04 +0530

    frontend built into single container

--- Docker/oneContainer/start.sh
+++ Docker/oneContainer/start.sh
@@ -6,5 +6,6 @@ java -jar /app/authn.jar > /app/authn.log 2>&1  &
 sleep 10
 java -jar /app/shortner.jar > /app/shortner.log 2>&1 &
 java -jar /app/redirector.jar > /app/redirector.log 2>&1 &
+nginx > /app/nginx.log 2>&1 &
 
 tail -f /app/*.log
