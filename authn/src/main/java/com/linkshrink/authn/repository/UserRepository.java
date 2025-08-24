459784e6a014e8637f0410247972332b572c1912
Alquama <alquamasalim000@gmail.com>
2025-08-25 00:32:27 +0530

    k8s deployment configs added

--- authn/src/main/java/com/linkshrink/authn/repository/UserRepository.java
+++ authn/src/main/java/com/linkshrink/authn/repository/UserRepository.java
@@ -4,11 +4,12 @@ import com.linkshrink.authn.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User,Integer> {
 
-    Optional<User> findByEmail(String email);
+    Optional<List<User>> findByEmail(String email);
 
 }
