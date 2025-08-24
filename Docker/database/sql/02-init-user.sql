5422ef1ac2af04751cf301ce16b6cc9e5397626b
Alquama <alquamasalim000@gmail.com>
2025-08-24 15:30:45 +0530

    dockerfile added for db

---
+++ Docker/database/sql/02-init-user.sql
@@ -0,0 +1,6 @@
+create role application;
+create schema linkshrink_sch authorization application;
+grant usage, select, insert, update, delete on all tables in schema
+linkshrink_sch to application;
+grant connect on database linkshrink to application;
+grant application to backend;
