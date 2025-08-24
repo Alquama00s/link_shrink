1699178a1ed92c7510eb7995b4ddda1c8c9ce014
Alquama <alquamasalim000@gmail.com>
2025-08-25 00:36:26 +0530

    fix

--- authn/src/main/java/com/linkshrink/authn/configurations/DefaultUserDetailsService.java
+++ authn/src/main/java/com/linkshrink/authn/configurations/DefaultUserDetailsService.java
@@ -15,7 +15,7 @@ public class DefaultUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userRepository.findByEmail(username).stream().findFirst().orElseThrow(()->
+        var user = userRepository.findByEmail(username).orElseThrow(()->
                 new UsernameNotFoundException("User not found")
         );
         return new PrivateUserDetails(user);
--- authn/src/main/java/com/linkshrink/authn/repository/UserRepository.java
+++ authn/src/main/java/com/linkshrink/authn/repository/UserRepository.java
@@ -4,12 +4,11 @@ import com.linkshrink.authn.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User,Integer> {
 
-    Optional<List<User>> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
