9bfb783cbe946d5edaf133549d6287706292eb5f
Alquama <alquamasalim000@gmail.com>
2025-08-24 19:32:43 +0530

    fix

---
+++ build-scripts/setupcache.sh
@@ -0,0 +1,7 @@
+set -eux
+minikube cache add alquama00s/linkshrink/authn/db
+minikube cache add alquama00s/linkshrink/redirector/db
+minikube cache add alquama00s/linkshrink/frontend
+minikube cache add alquama00s/linkshrink/shortner
+minikube cache add alquama00s/linkshrink/redirector
+minikube cache add alquama00s/linkshrink/authn
