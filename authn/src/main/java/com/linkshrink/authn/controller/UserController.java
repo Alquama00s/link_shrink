19fbac669e9804cb7cf0ccffe463433581e45493
Alquama <alquamasalim000@gmail.com>
2025-08-24 23:49:44 +0530

    k8s deployment configs added

--- authn/src/main/java/com/linkshrink/authn/controller/UserController.java
+++ authn/src/main/java/com/linkshrink/authn/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @Slf4j
-@CrossOrigin
+@CrossOrigin(originPatterns = "*")
 @RestController
 @RequestMapping("/api/user")
 @AllArgsConstructor
