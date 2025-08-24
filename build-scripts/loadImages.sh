5f1f6cbdc36bad4060ea4374d9c6fa716167ab2e
Alquama <alquamasalim000@gmail.com>
2025-08-24 19:14:54 +0530

    fix

--- build-scripts/loadImages.sh
+++ build-scripts/loadImages.sh
@@ -1,5 +1,6 @@
 set -eux
-minikube image load alquama00s/linkshrink/db
+minikube image load alquama00s/linkshrink/authn/db
+minikube image load alquama00s/linkshrink/redirector/db
 minikube image load alquama00s/linkshrink/frontend
 minikube image load alquama00s/linkshrink/shortner
 minikube image load alquama00s/linkshrink/redirector
