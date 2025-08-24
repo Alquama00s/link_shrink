297cf6c3334aa2d4f0bff77e6be71efa2e4c217b
Alquama <alquamasalim000@gmail.com>
2025-08-24 16:40:30 +0530

    k8s deployment configs added

--- build-scripts/loadImages.sh
+++ build-scripts/loadImages.sh
@@ -1,6 +1,6 @@
 set -eux
-kind load docker-image alquama00s/linkshrink/db --name $1
-kind load docker-image alquama00s/linkshrink/frontend --name $1
-kind load docker-image alquama00s/linkshrink/shortner --name $1
-kind load docker-image alquama00s/linkshrink/redirector --name $1
-kind load docker-image alquama00s/linkshrink/authn --name $1
+minikube cache add alquama00s/linkshrink/db
+minikube cache add alquama00s/linkshrink/frontend
+minikube cache add alquama00s/linkshrink/shortner
+minikube cache add alquama00s/linkshrink/redirector
+minikube cache add alquama00s/linkshrink/authn
